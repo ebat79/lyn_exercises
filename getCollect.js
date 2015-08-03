746a41283dac53a60dde5d8b16135bbb4371348c
Enrico Battiston <enrico.battiston@gmail.com>
2015-08-03 18:34:04 +0200

    exercise 8 completed

--- getCollect.js
+++ getCollect.js
@@ -1,11 +1,12 @@
 var http = require('http');
 var bl = require('bl');
-http.get(process.argv[2],function(result){
-    result.on('error', function(e){
-        console.log(e);
-    });
-    result.on('data', function(data){
-        bl.append(data);
-    })
-    console.log(bl.toString());
+var inUrl = process.argv[2];
+
+http.get(inUrl, function(result) {
+    result.pipe(bl(function(err,data) {
+        if (err) return console.error(err);
+        data = data.toString();
+        console.log(data.length);
+        console.log(data);
+    }));                
 });
